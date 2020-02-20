9aa218d5b6bf2de93eba8d0d537cb40fa509b86f
guillermo <you@example.com>
2020-02-20 13:31:34 +0100

    proyecto facturas terminado

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -11,44 +11,47 @@ import com.once.facturas.model.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * MainController es el controlador de la aplicación Facturas,
- * que es una aplicación tipo MVC (Modelo Vista Controlador)
+ * MainController es el controlador de la aplicación Facturas, que es una
+ * aplicación tipo MVC (Modelo Vista Controlador)
  * 
  * 
+ *//**
+ * el REQUESTMAPPING es una anotación que se encarga de relacionar un método con una petición
  */
+
 
 @RestController
 @RequestMapping(value = "/productos")
 class ProductoController {
-    
-    @Autowired
+
+    @Autowired                /**AUTOWIRED ES UNA ETIQUETA PARA LLAMAR AL REPOSITORIO,
+                               el repositorio es un lugar donde se guardan ciertas cosas como 
+                               los datos de las bases de datos */
     ProductoRepository pr;
 
-
-    @GetMapping("/")
-    public Iterable<Producto> getAllProductos(){
+    @GetMapping("/")                               
+    public Iterable<Producto> getAllProductos()   /** */
+     {
         return pr.findAll();
     }
 
-    @GetMapping("{id}/")
-    public Producto getProducto(
-            @PathVariable("id") Long id
-        ){
-            
-         try {
-            return pr.findById(id).get(); 
-         } catch (NoSuchElementException e) {
-             return null;
-         }
-            
+    @GetMapping("/{id}/")
+    public Producto getProducto(@PathVariable("id") Long id) {
+
+        try {
+            return pr.findById(id).get();
+        } catch (NoSuchElementException e) {
+            return null;
         }
 
+    }
 
     @GetMapping("/hello") // Escucho al GET en /hello
     @ResponseBody // Haré un body html para devolver la página completa
@@ -59,7 +62,13 @@ class ProductoController {
     @GetMapping("/count") // Escucho al GET en /count
     @ResponseBody // Haré un body html para devolver la página completa
     public String count() { // Método para devolver un string con responsebody
-        return "Tengo "+String.valueOf(pr.count())+" productos"; // Devuelvo el número de productos desde pr.count()
+        return "Tengo " + String.valueOf(pr.count()) + " productos"; // Devuelvo el número de productos desde pr.count()
     }
 
+    @PostMapping("/")
+    Producto crearProducto(@RequestBody Producto producto
+    ) {
+        Producto p = pr.save(producto);
+        return p;
+    }
 }
